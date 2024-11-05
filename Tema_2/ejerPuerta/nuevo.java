47375ce43e188c167584d96b2b8c8a99057cf969
ivan <icarreras19961@iesjoanramis.org>
2024-11-05 21:15:34 +0100

    ya casi

--- Tema_2/ejerPuerta/nuevo.java
+++ Tema_2/ejerPuerta/nuevo.java
@@ -13,21 +13,27 @@ public class nuevo {
         }
 
         public void run() {
+            boolean abierto = true;
 
             while (intentos <= 10) {
-                tiene = puerta.entrar(nuser);
-
+                
+                if (abierto) {
+                    abierto = false;
+                    tiene = puerta.entrar(nuser);
+                    abierto = true;
+                }
+                
                 intentos++;
                 try {
                     wait();
                 } catch (Exception e) {
-
                 }
 
                 if (tiene) {
                     System.out.println("El cliente " + nuser + " lo logro en: " + intentos);
                     break;
                 } else {
+
                     if (intentos >= 10) {
                         System.out.println("El cliente " + nuser + " se ha cansado de intentarlo");
                         break;
@@ -35,35 +41,29 @@ public class nuevo {
                 }
             }
         }
-    }
 
-    public static class Puerta {
-        boolean abierto = true;
+        public static class Puerta {
 
-        public synchronized boolean entrar(int nuser) {
-            if (abierto) {
-                abierto = false;
+            public synchronized boolean entrar(int nuser) {
                 if (productos > 0) {
-                    abierto = true;
                     productos--;
                     notifyAll();
                     return true;
                 }
-
+                return false;
             }
-            return false;
         }
-    }
 
-    public static void main(String[] args) {
-        // Bucle de 300
-        for (int i = 0; i < 300; i++) {
-            Almacen cliente = new Almacen();
-            cliente.setNuser(i);
+        public static void main(String[] args) {
+            // Bucle de 300
+            for (int i = 0; i < 300; i++) {
+                Almacen cliente = new Almacen();
+                cliente.setNuser(i);
 
-            cliente.start();
+                cliente.start();
+            }
+            System.out.println("Ya esta");
+
         }
-        System.out.println("Ya esta");
-
     }
 }
